65ef3e0f7299318ed1b03dc0951e1184ca5b82ed
catwine <linzeyu@my.swjtu.edu.cn>
2023-09-24 22:22:12 +0800

    commit

--- Homework1/1.20.cpp
+++ Homework1/1.20.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-typedef struct Polyn
-{
-    int a;
-    struct Polyn *next;
-} PolynTerm, *PolynPtr;
-int n, x, res;
+int cal(int n, int x, int a[]);
+const int N = 1e2;
+int n, x, a[N];
 int main()
 {
-    PolynPtr head = new PolynTerm, p = head; // 1次
-    cin >> n;                                // 1次
-    for (int i = 0; i <= n; ++i)             // n + 2次
-    {
-        PolynPtr tmp = new PolynTerm; // n + 1次
-        cin >> tmp->a;                // n + 1次
-        p->next = tmp, p = p->next;   // n + 1次
-    }
-    p->next = nullptr, p = head; // 1次
-    cin >> x;                    // 1次
-    for (int i = 0; i <= n; ++i) // n + 2次
-    {
-        p = p->next;             // n + 1次
-        res += p->a * pow(x, i); // n + 1次
-    }
-    cout << res << '\n'; // 1次
-    return 0;
+	cin >> n >> x;
+	for (int i = 0; i < n; ++i)
+		cin >> a[i];
+	cout << cal(n, x, a) << '\n';
+	return 0;
+}
+int cal(int n, int x, int a[])
+{
+	int res = 0;
+	for (int i = 0; i < n; ++i)
+		res += pow(x, i) * a[i];
+	return res;
 }
